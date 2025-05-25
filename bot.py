68cc4c73f13eb974de3f0b1b7b256f406fa91e1e
LordTakashi3535 <post.vielikogo@gmail.com>
2025-05-25 10:04:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("ТвояТаблица").sheet1
